321d00dc25e35da96f89c406e486d516c74fc332
IgorPR0 <igortecnologia2020@gmail.com>
2020-10-07 22:37:26 -0300

    Trainig DateTime

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿    using System;
+
+namespace UltimaParcela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Valor , Divisao , Soma;
+
+            DateTime Data = DateTime.Now;
+            DateTime Hora;
+
+            Console.Write("Digite o valor a ser pago :");
+            Valor = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o numero de Parcela :");
+            Divisao = Convert.ToDouble(Console.ReadLine());
+
+            Soma = Valor / Divisao;
+
+            Hora = Data.AddMonths(5);
+            Console.WriteLine($"A Data de parcela é : {Hora}, No valor total de R${Soma}");
+        
+        }
+    }
+}
